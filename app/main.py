6798adc920167586dd8bd6259ecb3816dcb41dbe
Yash Panwar <yashpanwar01102003@gmail.com>
2025-08-23 18:56:12 +0530

    Added Cors policies

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import resume_routes, career_routes
 
 app = FastAPI(title="AI Resume & Career Guidance API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 app.include_router(resume_routes.router)
 app.include_router(career_routes.router)
